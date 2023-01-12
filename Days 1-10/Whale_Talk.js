5e12997d7ac92b1dff71191d95a3219d7891e7ea
a20z <88807971+a20z@users.noreply.github.com>
2023-01-12 11:50:10 -0800

    update

---
+++ Days 1-10/Whale_Talk.js
@@ -0,0 +1,25 @@
+// Codecademy project for turning speech into whale talk
+
+
+let input = 'Dark Souls is the best';
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++){
+  if (input[i] === 'e'){
+    resultArray.push(input[i]);
+  }
+  if (input[i] === 'u'){
+    resultArray.push(input[i]);
+  }
+  for (let j = 0; j < vowels.length; j++){
+    if (input[i] === vowels[j]){
+      resultArray.push(input[i]);
+    }
+  } 
+}
+
+const resultString = resultArray.join('').toUpperCase();
+console.log(resultString);
