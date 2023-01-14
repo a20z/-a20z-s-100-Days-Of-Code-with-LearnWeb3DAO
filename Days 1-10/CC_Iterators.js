2a2d5b254ad15a1d0b038b49254466871158ba50
a20z <88807971+a20z@users.noreply.github.com>
2023-01-13 21:02:24 -0800

    upadate

--- Days 1-10/CC_Iterators.js
+++ Days 1-10/CC_Iterators.js
@@ -130,3 +130,24 @@ const newSum = newNumbers.reduce(function(accumulator, currentValue){
    
   
 // Exercise 8 - Iterator documentation
+
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+console.log('hola');
+
+// Something is missing in the method call below
+
+console.log(words.some(() => {
+  return word.length < 6;
+}));
+
+// Use filter to create a new array
+
+
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+// console.log(interestingWords.every((word) => { } ));
+
+
+// Look at exercise 8 again
