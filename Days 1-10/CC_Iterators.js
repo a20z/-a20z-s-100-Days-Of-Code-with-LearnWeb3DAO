4544aea97b1a4f9641fe2548fa95ee6ad4cde2c1
a20z <88807971+a20z@users.noreply.github.com>
2023-01-14 20:16:13 -0800

    update

--- Days 1-10/CC_Iterators.js
+++ Days 1-10/CC_Iterators.js
@@ -140,14 +140,3 @@ console.log('hola');
 console.log(words.some(() => {
   return word.length < 6;
 }));
-
-// Use filter to create a new array
-
-
-
-// Make sure to uncomment the code below and fix the incorrect code before running it
-
-// console.log(interestingWords.every((word) => { } ));
-
-
-// Look at exercise 8 
