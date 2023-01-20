732bf13a479b2fda8ebe9223ef2c7ba312d496a9
a20z <88807971+a20z@users.noreply.github.com>
2023-01-19 22:50:15 -0800

    UPDATE

--- Days 11-20/CC_Advanced-Objects.js
+++ Days 11-20/CC_Advanced-Objects.js
@@ -41,4 +41,15 @@ robot.checkEnergy();
 
 // Exerecise 3 - privacy 
 
+const robot = {
+  _energyLevel: 100,
+  recharge(){
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+  }
+};
 
+
+robot._energyLevel = 'high';
+
+robot.recharge();
