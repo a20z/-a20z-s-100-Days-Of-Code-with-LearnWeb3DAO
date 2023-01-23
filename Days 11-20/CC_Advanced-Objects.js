2c8c550f8ee9127d50cc86868507bbf3f8d7671e
a20z <88807971+a20z@users.noreply.github.com>
2023-01-22 20:12:18 -0800

    update

--- Days 11-20/CC_Advanced-Objects.js
+++ Days 11-20/CC_Advanced-Objects.js
@@ -112,3 +112,59 @@ const robot = {
 robot.numOfSensors = 100;
 
 console.log(robot.numOfSensors);
+
+// Execrise 6 -  Factory functions
+
+const robotFactory = (model, mobile) =>{
+  return {
+    model: model,
+    mobile: mobile,
+    beep () {
+      console.log('Beep Boop');
+    }
+  };
+  
+}; 
+
+const tinCan = robotFactory('P-500', true);
+
+tinCan.beep();
+
+// Exercise 7 - Property Value Shorthand
+
+const robotFactory = (model, mobile) => {
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
+
+// Exercise 8 - Destructed Assignment
+
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log('Beep Boop');
+    },
+    fireLaser() {
+      console.log('Pew Pew');
+    },
+  }
+};
+
+// extracts object property to variable
+const { functionality } = robot;
+
+functionality.beep();
+
+// Exercise 9 - 
