bc20b3853d58ce939009722a837b3447c748c241
a20z <88807971+a20z@users.noreply.github.com>
2023-01-23 14:04:46 -0800

    update

--- Days 11-20/Meal_Maker.js
+++ Days 11-20/Meal_Maker.js
@@ -1,1 +1,26 @@
 // Codecademy's Meal maker project
+let menu = {
+    _meal: '',
+    _price: 0,
+    set meal (mealToCheck){
+      if (typeof mealToCheck === 'string'){
+        return this._meal = mealToCheck;
+      }
+    },
+   set price (priceToCheck){
+     if (typeof priceToCheck === 'number'){
+       return this._price = priceToCheck;
+     }
+   },
+    get todaysSpecial () {
+      if (this._meal && this._price){
+        return `Today's special is ${this._meal} for only ${this._price} ETH!`
+    } else return "Meal or price was not set correctly!"
+   }
+  };
+  
+  menu.meal = 'Tacos';
+  menu.price = 3;
+  
+  console.log(menu.todaysSpecial);
+  
