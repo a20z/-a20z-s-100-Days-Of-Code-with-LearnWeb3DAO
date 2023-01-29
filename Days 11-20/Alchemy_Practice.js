530dff6d3545577345453127d620912c1388fac6
a20z <88807971+a20z@users.noreply.github.com>
2023-01-28 23:45:57 -0800

    update

--- Days 11-20/Alchemy_Practice.js
+++ Days 11-20/Alchemy_Practice.js
@@ -1,3 +1,30 @@
-// intermediate JS concepts
-//intermediate concepts
-// JavaScript is awesome
+// Objects Add  By type
+const ORDER_TYPES = {
+    PIZZA: 0,
+    WINGS: 1,
+    SALAD: 2,
+    BURGER: 3,
+    FRIES: 4,
+    SANDWICH: 5,
+}
+
+module.exports = ORDER_TYPES;
+
+// numberOfPizzas.js
+
+const ORDER_TYPES = require('./orderTypes');
+
+function numberOfPizzas(orders) {
+    let total = 0;
+    for (let i in orders) {
+        if (orders[i].type == ORDER_TYPES.PIZZA){
+            total += orders[i].pizzas;
+        }
+
+        
+
+    }
+    return total;
+}
+
+module.exports = numberOfPizzas;
