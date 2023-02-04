d1bceb8ebfe78edcf22fad69794a0169fa336fc2
a20z <88807971+a20z@users.noreply.github.com>
2023-02-04 14:40:42 -0800

    update

--- Days 21-30/Alchemy_Classes_Prototypes.js
+++ Days 21-30/Alchemy_Classes_Prototypes.js
@@ -6,3 +6,23 @@ function thisName() {
     return this.name;
 
 }
+
+// Binding this keyword to a function
+function thisName() {
+    return this.name;
+
+}
+
+module.exports = thisName.bind({name: 'Bob'});
+
+// Implicit binding - Call Site
+
+const obj = {
+    name: 'Bob',
+    getName: function() {
+        return obj.name;
+    }
+}
+
+// Unbound function 
+
