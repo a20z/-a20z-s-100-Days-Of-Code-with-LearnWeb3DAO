8188ac6da092ad9850702406c8f9e917a0748f3d
a20z <88807971+a20z@users.noreply.github.com>
2023-02-06 12:55:00 -0800

    update

--- Days 31-40/Alchemy_Prototypes.js
+++ Days 31-40/Alchemy_Prototypes.js
@@ -41,3 +41,61 @@ Shape.prototype.move = function (x,y){
 module.exports = Shape;
 
 // Cricle Shape
+
+const Shape = require('./Shape');
+
+function Circle(x, y, radius) {
+    Shape.call(this, x, y);
+    // store radius on this
+    this.radius = radius;
+}
+
+module.exports = Circle;
+
+// Move Circle
+
+const Shape = require('./Shape');
+
+function Circle(x, y, radius) {
+    Shape.call(this, x, y);
+    // store radius on this
+    this.radius = radius;
+}
+
+Circle.prototype = Object.create(Shape.prototype);
+
+module.exports = Circle;
+
+// Rectangle
+
+const Shape = require('./Shape');
+
+function Rectangle(x, y, height, width) {
+    Shape.call(this, x, y);
+    this.width = width;
+    this.height = height;
+}
+
+Rectangle.prototype = Object.create(Shape.prototype);
+
+module.exports = Rectangle;
+
+// Rectangle flip
+
+const Shape = require('./Shape');
+
+function Rectangle(x, y, height, width) {
+    Shape.call(this, x, y);
+    this.width = width;
+    this.height = height;
+}
+
+Rectangle.prototype = Object.create(Shape.prototype);
+
+Rectangle.prototype.flip = function(){
+    const width = this.width;
+    this.width = this.height;
+    this.height = width;
+}
+
+module.exports = Rectangle;
