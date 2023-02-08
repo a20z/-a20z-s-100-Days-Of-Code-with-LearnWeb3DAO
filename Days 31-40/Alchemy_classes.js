9fb1545815e1b20fa6f7d616cfe125d399b672bd
a20z <88807971+a20z@users.noreply.github.com>
2023-02-07 23:02:27 -0800

    update

--- Days 31-40/Alchemy_classes.js
+++ Days 31-40/Alchemy_classes.js
@@ -43,3 +43,14 @@ module.exports = Warrior;
 // Super Warrior
 
 // Add range: Add a rage property to the Warrior. The value of rage will start at 0.
+
+const Hero = require('./Hero');
+
+class Warrior extends Hero {
+    constructor(){
+        super();
+        this.rage = 0;
+    }
+    
+}
+
