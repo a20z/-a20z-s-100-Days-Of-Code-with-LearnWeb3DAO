ba5dd6898e74d2ec288b39797e5e86c888976bab
a20z <88807971+a20z@users.noreply.github.com>
2023-02-16 23:20:50 -0800

    update

---
+++ Days 41-50/Alchemy_array_map.js
@@ -0,0 +1,10 @@
+// Alchemy University course on array map
+
+
+/* 
+The .map() method creates a new array populated with the results of calling a provided function on
+every element in the calling array
+
+
+
+*/ 
