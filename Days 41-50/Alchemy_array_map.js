8c0909591b303500fbce1c1db30e4d8807672b18
a20z <88807971+a20z@users.noreply.github.com>
2023-02-17 00:20:04 -0800

    update

--- Days 41-50/Alchemy_array_map.js
+++ Days 41-50/Alchemy_array_map.js
@@ -4,7 +4,20 @@
 /* 
 The .map() method creates a new array populated with the results of calling a provided function on
 every element in the calling array
-
-
-
 */ 
+
+//Map function 
+
+// Take the array arr and add one to every element, retuen the resultin array
+function plusOne(arr) {
+    return arr.map(function(x){
+        return x + 1;
+    })
+}
+
+module.exports = plusOne;
+
+//Mapping a function
+
+// Using the SquareRoot function, map each element in the array arr to its square root, return new array
+
