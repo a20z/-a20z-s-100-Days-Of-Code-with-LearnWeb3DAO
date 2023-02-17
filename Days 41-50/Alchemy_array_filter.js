83f392176ddc5e3dadf2566634c572071fb559f6
a20z <88807971+a20z@users.noreply.github.com>
2023-02-17 03:01:48 -0800

    update

--- Days 41-50/Alchemy_array_filter.js
+++ Days 41-50/Alchemy_array_filter.js
@@ -8,3 +8,21 @@ function lessThanFive(array) {
         }
     ));
 }
+
+// Only True
+// Given an array of booleans, return only true
+
+function onlyTrue(array) {
+    return array.filter((element) =>{
+        return element;
+    })
+}
+
+// Filtering strings 
+// Given and array of strings, return strings whose length is at most 3
+
+function shortStrings(array) {
+    return array.filter((function(x){
+        return x.length <= 3
+    }));
+}
