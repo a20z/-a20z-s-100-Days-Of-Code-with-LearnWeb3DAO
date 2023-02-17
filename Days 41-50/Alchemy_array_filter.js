6f80446fb9c7f08d0c19a423d5dd9267f2f3292f
a20z <88807971+a20z@users.noreply.github.com>
2023-02-17 02:44:49 -0800

    update

--- Days 41-50/Alchemy_array_filter.js
+++ Days 41-50/Alchemy_array_filter.js
@@ -1,1 +1,10 @@
 // Alchemy array filter
+
+// Given an array of elements, find the elements whose value is < 5
+
+function lessThanFive(array) {
+    return array.filter((function(x){
+        return x < 5;
+        }
+    ));
+}
