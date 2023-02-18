5059596d4c9b649d194955f5da393f28173d8d70
a20z <88807971+a20z@users.noreply.github.com>
2023-02-17 19:21:43 -0800

    update

---
+++ Days 41-50/Alchemy_array_reduce.js
@@ -0,0 +1,19 @@
+// Alchemy Array Reduce
+
+/*
+The reduce() method executes a user-supplied "reducer" callback function on each element of thje array,
+in order, passing the return value from the calculation on the preceding element. The final result of 
+running the reducer across all elements of the array is a single value
+
+The first time that the callebakc is run, there is no "return value of the previous calculation". 
+If supplied, an initial value may br used in its place. Otherwise the array element at index 0 is
+used as the initial valu and iteration starts from the next element (index 1 instead of 0)
+
+WHEN SHOULD YOU USE REDUCE? 
+
+Use the reduce method when you haqve an array of elements you'd like to combine into a 
+single value or object
+*/
+
+// Array reduce
+
