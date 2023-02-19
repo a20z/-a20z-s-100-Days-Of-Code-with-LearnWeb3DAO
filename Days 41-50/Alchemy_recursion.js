ed0eef676a36bcc955d4bf47ac6525ec7dc3dbb9
a20z <88807971+a20z@users.noreply.github.com>
2023-02-18 23:57:09 -0800

    update

--- Days 41-50/Alchemy_recursion.js
+++ Days 41-50/Alchemy_recursion.js
@@ -1,5 +1,10 @@
 // Alchem,y recursion
 
 /* Generally, iteration is more performant and can be less confusing for simple problems. 
-Therefore, we should only look 
+Therefore, we should only look to use recursion in cases where it helps creat clear and consise code.
+
+Recursion is best used in cases where a problem can be subdivided into many smaller prtoblems,
+a command-and-conquer approach. 
+
+That's why you typically see recursion mentioned with tree-like data structures
 */
