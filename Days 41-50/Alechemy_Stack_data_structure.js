025b1dfaa99a807c7afb75242c8c607de9aa2d84
a20z <88807971+a20z@users.noreply.github.com>
2023-02-19 02:16:57 -0800

    update

--- Days 41-50/Alechemy_Stack_data_structure.js
+++ Days 41-50/Alechemy_Stack_data_structure.js
@@ -1,3 +1,7 @@
 // Alchemy Stack Data Structure
 
 // Push & Pop
+
+// .push() - the push method adds one or more elements to the ned of array & returns new array length
+// .pop() - reomeves the LAST element from array & returns that element, changing length of array
+// 
