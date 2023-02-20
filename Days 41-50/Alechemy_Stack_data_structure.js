f5bd3f281d6c09025f185bb303ee8cf9be567402
a20z <88807971+a20z@users.noreply.github.com>
2023-02-20 09:12:09 -0800

    update

--- Days 41-50/Alechemy_Stack_data_structure.js
+++ Days 41-50/Alechemy_Stack_data_structure.js
@@ -4,4 +4,42 @@
 
 // .push() - the push method adds one or more elements to the ned of array & returns new array length
 // .pop() - reomeves the LAST element from array & returns that element, changing length of array
-// isEmpty()  
+
+const { MAX_STACK_SIZE } = require('./config');
+
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+    push(item) {
+        this.items.push(item);
+    }
+    pop() {
+        return this.items.pop();
+    }
+    isEmpty() {
+        
+    }
+    peek() {
+        
+    }
+}
+
+// Overflow & Underflow
+
+/*
+Recursive call stakc error
+
+when dealingwith recusion, may run ionto a case where the call stack overflows
+
+Ex:
+
+function randomFunction(){
+    randomFunction();
+}
+//make the initial call
+myFunciton();
+
+After that intiial call, this program will never terminate. Eventially the call stack 
+will overflow because it will run out of memory to hold where to return program execution to
+*/
