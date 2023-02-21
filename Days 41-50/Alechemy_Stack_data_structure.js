62c9d9984cf02ae06ec40815bea8b06810ea0be1
a20z <88807971+a20z@users.noreply.github.com>
2023-02-20 23:54:39 -0800

    update

--- Days 41-50/Alechemy_Stack_data_structure.js
+++ Days 41-50/Alechemy_Stack_data_structure.js
@@ -43,3 +43,31 @@ myFunciton();
 After that intiial call, this program will never terminate. Eventially the call stack 
 will overflow because it will run out of memory to hold where to return program execution to
 */
+
+const { MAX_STACK_SIZE } = require('./config');
+
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+    push(item) {
+        if (this.items.length === MAX_STACK_SIZE) {
+            throw new Error("Stack Overflow!");
+        }
+        this.items.push(item);
+    }
+    pop() {
+        if (this.items.length === 0) {
+            throw new Error("Stack Underflow");
+        }
+        return this.items.pop();
+    }
+    isEmpty() {
+        
+    }
+    peek() {
+        
+    }
+}
+
+// isEmpty and Peed
