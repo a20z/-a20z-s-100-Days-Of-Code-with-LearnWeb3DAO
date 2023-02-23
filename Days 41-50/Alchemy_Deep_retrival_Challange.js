13dc19404353f8c03a5fb48ad4380283a00252ef
a20z <88807971+a20z@users.noreply.github.com>
2023-02-22 23:53:34 -0800

    update

---
+++ Days 41-50/Alchemy_Deep_retrival_Challange.js
@@ -0,0 +1,21 @@
+// Alchemy Deep retreiveal challange
+/*
+Given an object which contains an arbritrary number of objects, find the deeply nested value
+The Goal is to take an object like:
+
+{
+    prop: {
+        prop: {
+            prop: 3
+        }
+    }
+}
+
+And return 3. You will not know hao deep the value is nested, only that it will be nested under an
+arbritraty number of prop attributes.
+
+typeof operator will be usef ul in this challange
+
+--The typeof operator returns a string indicating the type of the operands value
+
+*/
