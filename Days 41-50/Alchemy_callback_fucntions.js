c9f801effafa6370581b3e3ceaaf7a3daf359b76
a20z <88807971+a20z@users.noreply.github.com>
2023-02-24 13:37:39 -0800

    update

--- Days 41-50/Alchemy_callback_fucntions.js
+++ Days 41-50/Alchemy_callback_fucntions.js
@@ -1,1 +1,59 @@
 // Alchemy Callback functions
+
+
+// Running a callback function
+
+/**
+ * Runs a callback function immediately
+ * @param {function} callbackFunction
+ */
+function runCallback(callbackFunction) {
+    callbackFunction();
+}
+
+// Asynchronous Callback
+
+/*
+The global setTimeout() method sets a timer which executes a function or a specified
+peice of code once the timer erxpires
+*/
+
+function runCallback(callbackFunction) {
+    setTimeout(() => {
+        callbackFunction();
+    }, 1000);
+}
+
+// Dialog Callback
+
+class Dialog {
+    constructor() {
+        this.callbacks = []
+    }
+    onClose(callbackFunction) {
+        // store the callback
+        this.callbacks.push(callbackFunction);
+    }
+
+    close() {
+        let i = -1
+        while (++i < this.callbacks.length)
+            this.callbacks[i]()
+    }
+}
+
+// Multiple dialog callbacks
+
+class Dialog {
+    constructor() {
+        this.callbackFunctions = [];
+    }
+    close() {
+        this.callbackFunctions.forEach((callbackFn) => {
+            callbackFn();
+        });
+    }
+    onClose(callbackFunction) {
+        this.callbackFunctions.push(callbackFunction);
+    }
+}
