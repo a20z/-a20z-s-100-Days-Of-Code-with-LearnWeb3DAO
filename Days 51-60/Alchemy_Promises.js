1b198a9d6b9676fba0ae92298b43874323860bec
a20z <88807971+a20z@users.noreply.github.com>
2023-02-26 01:50:22 -0800

    update

--- Days 51-60/Alchemy_Promises.js
+++ Days 51-60/Alchemy_Promises.js
@@ -2,3 +2,40 @@
 
 // Using Promises
 
+const { makeFood } = require('./Kitchen');
+
+class Order {
+    constructor() {
+        this.isReady = false;
+    }
+    request(food) {
+        makeFood(food).then(() => {
+            this.isReady = true;
+        });
+    }
+}
+
+// Catch Error
+
+const { makeFood } = require('./Kitchen');
+
+class Order {
+    /*Constructor method initializes a isReady to false */
+    constructor() {
+        this.isReady = false;
+    }
+    request(food) { /*request method is used to request a certain type of food 
+        by passing food argument and uses function make food */
+        makeFood(food).then(() => {
+            /*If the promise returned by makeFood resolves successfully, the request method sets the
+            is ready property of the Order instance to true, indicating food is ready */
+            this.isReady = true;
+            /*If promise returened by makeFood rejects with an error, the request method sets the error propert 
+            of the Order instance to the error that was thrown durinf the prep process */
+        }).catch((error) => {
+            this.error = error;
+        });
+    }
+}
+
+// Create a promise
