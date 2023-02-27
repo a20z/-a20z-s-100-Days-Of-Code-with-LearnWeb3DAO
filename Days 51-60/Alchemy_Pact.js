8646144ab08c8562ccf06b728e21be9474129fea
a20z <88807971+a20z@users.noreply.github.com>
2023-02-27 01:30:01 -0800

    update

---
+++ Days 51-60/Alchemy_Pact.js
@@ -0,0 +1,33 @@
+// Alchemy Pact: A promise library
+
+class Pact {
+    // add methods to return on the instance
+    then(){
+
+    }
+
+    catch(){
+        
+    }
+}
+
+// The executor
+class Pact {
+    constructor(fn) {
+        this.resolve = () => {
+
+        }
+        this.reject = () => {
+            
+        }
+        fn(this.resolve, this.reject);
+    }
+    then() {
+        
+    }
+    catch() {
+        
+    }
+}
+
+// Then ... What?
