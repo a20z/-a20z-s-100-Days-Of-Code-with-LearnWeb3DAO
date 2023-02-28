a2b5d56112a28f5fb489902eec1febf4e3cd16eb
a20z <88807971+a20z@users.noreply.github.com>
2023-02-27 23:56:14 -0800

    update

--- Days 51-60/Alchemy_Cryptographic_Hashes.js
+++ Days 51-60/Alchemy_Cryptographic_Hashes.js
@@ -12,3 +12,5 @@ function findColor(hash) {
     return COLORS.find(x => toHex(sha256(utf8ToBytes(x))) === toHex(hash));
 }
 
+/*The find() method returns the first element in the provided array that satisfies 
+the provided testing function. If no values satisfy the testing function, undefined is returned.*/
