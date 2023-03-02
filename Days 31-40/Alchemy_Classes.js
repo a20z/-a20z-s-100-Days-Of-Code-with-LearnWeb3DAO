58c4b6079fbb0f1977e9f6414ca83802855ba04a
Alex Zayago <a20zc0x@gmail.com>
2023-03-01 18:45:58 -0800

    update

---
+++ Days 31-40/Alchemy_Classes.js
@@ -0,0 +1,14 @@
+// Alchemy U- JavaScript classes
+
+// 1. Hero
+
+// Within your constructor function, add a health property to a hero instance and set it to 50.
+
+class Hero {
+    constructor() {
+        this.health = 50;
+    }
+}
+
+
+
