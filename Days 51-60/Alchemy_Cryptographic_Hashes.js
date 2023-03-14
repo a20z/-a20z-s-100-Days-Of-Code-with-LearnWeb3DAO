2f477228c93471f458e959495b4c7a8a2e90e143
a20z <88807971+a20z@users.noreply.github.com>
2023-03-13 21:29:43 -0700

    update

--- Days 51-60/Alchemy_Cryptographic_Hashes.js
+++ Days 51-60/Alchemy_Cryptographic_Hashes.js
@@ -16,3 +16,52 @@ function findColor(hash) {
 the provided testing function. If no values satisfy the testing function, undefined is returned.*/
 
 
+
+<<<<<<< HEAD
+do {
+    // Hashing messeges
+    
+    const { keccak
+} while (do {
+    condition
+} while (condition););256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+
+function hashMessage(message) {
+    const byteMessage = utf8ToBytes(message);
+    const hash = keccak256(byteMessage);
+    return hash;
+}
+
+// Signing the hash
+
+const secp = require("ethereum-cryptography/secp256k1");
+const hashMessage = require('./hashMessage');
+
+const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+
+async function signMessage(msg) {
+    const messageHash = hashMessage(msg);
+    return secp.sign(messageHash, PRIVATE_KEY, { recovered: true });
+}
+
+// Recover the Public key
+const secp = require("ethereum-cryptography/secp256k1");
+const hashMessage = require("./hashMessage");
+
+async function recoverKey(message, signature, recoveryBit) {
+    const messageHash = hashMessage(message);
+    return secp.recoverPublicKey(messageHash, signature, recoveryBit);
+}
+
+//KEY TO ADDRESS
+
+const secp = require("ethereum-cryptography/secp256k1");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+
+function getAddress(publicKey) {
+    // the first byte indicates whether this is in compressed form or not
+    return keccak256(publicKey.slice(1)).slice(-20);
+}
+=======
+>>>>>>> 61e4d9e765a2938336ece6a6f4a62a5b497530aa
--- Days 61-70/Alchemy_BlockchainStructure.js
+++ Days 61-70/Alchemy_BlockchainStructure.js
@@ -1,2 +1,45 @@
 // Alchemy Blockchain Structure unit
 
+
+// Blockchain.js
+
+const Block = require('./Block');
+
+class Blockchain {
+    constructor() {
+        this.chain = [new Block()];
+    }
+
+    addBlock(block) {
+        block.previousHash = this.chain[this.chain.length - 1].toHash();
+        this.chain.push(block);
+    }
+
+    isValid() {
+        for(let i = this.chain.length - 1; i > 0; i--) {
+            const block = this.chain[i];
+            const prev = this.chain[i - 1];
+            if(block.previousHash.toString() !== prev.toHash().toString()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+module.exports = Blockchain;
+
+// Block.js
+
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor (data){
+        this.data = data;
+    }
+    toHash() {
+        return SHA256(this.data + this.previousHash) // a hash!
+    }
+}
+
+module.exports = Block;
